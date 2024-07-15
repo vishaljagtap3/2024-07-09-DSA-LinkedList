3e5eb960978134716b1875208bf7c4a6c2873b3f
vishal <vishal.jagtap@bitcode.in>
2024-07-15 15:07:59 +0530

    Complete linked list

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -5,6 +5,10 @@ public class LinkedList {
         head = null;
     }
 
+    public Node getHead() {
+        return head;
+    }
+
     public boolean insert(int data) {
 
         Node newNode = new Node(data);
@@ -30,13 +34,131 @@ public class LinkedList {
         return true;
     }
 
+    public boolean insert(int data, int position) {
+        Node newNode = new Node(data);
+        if (newNode == null) {
+            return false;
+        }
+
+        //if position is one
+        if (position == 1) {
+            newNode.setNext(head);
+            head = newNode;
+            return true;
+        }
+
+        Node prev = head;
+        for(int i = 1; i < position - 1; i++) {
+            prev = prev.getNext();
+            if(prev == null) {
+                return false;
+            }
+        }
+
+        newNode.setNext(prev.getNext());
+        prev.setNext(newNode);
+
+        return true;
+    }
+
     public void display() {
         Node temp = head;
 
-        while(temp != null) {
+        while (temp != null) {
             System.out.print(temp.getData() + " ");
             temp = temp.getNext();
         }
         System.out.println();
     }
+
+    public void display(Node head) {
+        if(head == null) {
+            System.out.println();
+            return;
+        }
+        System.out.print(head.getData() + " ");
+        display(head.getNext());
+    }
+
+    public void displayReverse(Node head) {
+        if(head == null) {
+            System.out.println();
+            return;
+        }
+        displayReverse(head.getNext());
+        System.out.print(head.getData() + " ");
+
+    }
+
+    public void reverse() {
+        if(head == null) {
+            return;
+        }
+
+        Node n1 = head;
+        Node n2 = head.getNext();
+        Node n3;
+
+        while(n2 != null) {
+            n3 = n2.getNext();
+            n2.setNext(n1);
+            n1 = n2;
+            n2 = n3;
+        }
+
+        head.setNext(null);
+        head = n1;
+    }
+
+    public boolean deleteByPosition(int position) {
+        if(head == null || position <= 0) {
+            return false;
+        }
+
+        if(position == 1) {
+            head = head.getNext();
+            return true;
+        }
+
+        //locate the node at position - 1
+        Node prev = head;
+        for(int i = 1; i < position - 1; i++) {
+            prev = prev.getNext();
+            if(prev.getNext() == null) {
+                return false;
+            }
+        }
+
+        Node del = prev.getNext();
+        prev.setNext(del.getNext());
+
+        return true;
+    }
+
+    public boolean deleteByVal(int data) {
+        if(head == null) {
+            return false;
+        }
+
+        if(head.getData() == data) {
+            head = head.getNext();
+            return true;
+        }
+
+        Node prev = head;
+        Node del = head;
+
+        while(del.getData() != data) {
+            prev = del;
+            del = del.getNext();
+            if(del == null) {
+                return false;
+            }
+        }
+
+        prev.setNext(del.getNext());
+        return true;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,50 @@ public class Main {
         l1.display();
         l1.insert(50);
         l1.display();
+        l1.insert(60, 1);
+        l1.display();
+        l1.insert(70, 4);
+        l1.display();
+        l1.insert(80, 8);
+        l1.display();
+        System.out.println( l1.insert(90, 10) );
+        l1.display();
+        l1.display(l1.getHead());
+        l1.displayReverse(l1.getHead());
+        System.out.println();
 
+        l1.reverse();
+        l1.display();
+
+        l1.insert(80, 4);
+        l1.insert(80, 6);
+        l1.insert(80);
+        l1.display();
+
+        while (l1.deleteByVal(80) )
+            ;
+
+        l1.display();
+
+        /*l1.deleteByVal(80);
+        l1.display();
+        l1.deleteByVal(30);
+        l1.display();
+        l1.deleteByVal(60);
+        l1.display();
+        l1.deleteByVal(60);
+        l1.display();*/
+
+
+        /*l1.deleteByPosition(1);
+        l1.display();
+        l1.deleteByPosition(4);
+        l1.display();
+        l1.deleteByPosition(6);
+        l1.display();
+        boolean isDeleted = l1.deleteByPosition(6);
+        System.out.println(isDeleted);
+        l1.display();*/
 
     }
 }
